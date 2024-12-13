e440ac3c01e1118261b99036ff8baaddad4ea90e
Jairo-18 <jhonlegarda1.2@gmail.com>
2024-12-13 03:15:23 -0500

    Fix: project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
